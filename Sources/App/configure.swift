f6b4fd92ef56ec000a7e856723fe2c41f12b4215
Dawid Nadolski <dawidnadolski@icloud.com>
2021-08-22 18:29:42 +0200

    Initial commit

---
+++ Sources/App/configure.swift
@@ -0,0 +1,27 @@
+import Fluent
+import FluentSQLiteDriver
+import FluentPostgresDriver
+import Leaf
+import Vapor
+
+// configures your application
+public func configure(_ app: Application) throws {
+    // uncomment to serve files from /Public folder
+    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+					
+	app.databases.use(
+		.postgres(
+			hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+			username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
+			password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
+			database: Environment.get("DATABASE_NAME") ?? "vapor_database"
+		),
+		as: .psql
+	)
+	
+	app.logger.logLevel = .debug
+	
+	try app.autoMigrate().wait()
+
+    try routes(app)
+}
---
+++ Sources/App/routes.swift
@@ -0,0 +1,8 @@
+import Fluent
+import Vapor
+
+func routes(_ app: Application) throws {
+    app.get { req in
+        return "Hello world!"
+    }
+}
